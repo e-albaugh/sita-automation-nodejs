c3bc4707e1313ef64b6e4080028cc3a856855872
e-albaugh <51415624+e-albaugh@users.noreply.github.com>
2024-11-14 06:17:26 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,29 +1,30 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { authenticateAndGetSessionCookie, updateManualActionStatus, startQualityGate } from './src/updateValueEdge.js';
+import { authenticateAndGetSessionCookie, updateManualActionStatus/*, startQualityGate*/ } from './src/updateValueEdge.js';
 import { checkSonarStatus } from './src/checkSonarStatus.js';
-import { takeScreenshot } from './src/takeScreenshot.js';
+/*import { takeScreenshot } from './src/takeScreenshot.js';*/
 
 async function main() {
     try {
         console.log("Starting authentication...");
         const cookie = await authenticateAndGetSessionCookie();
         console.log("Received LWSSO_COOKIE_KEY:", cookie);
-        /*
+        
         console.log("Proceeding with SonarQube status check...");
         const sonarStatus = await checkSonarStatus();
         console.log("SonarQube Status:", sonarStatus);
-        */ 
+
+        /*
         console.log("Initializing SonarQube Screenshot...");
         await takeScreenshot();
-
+        */
         console.log("Beginning update to Manual Action...")
         await updateManualActionStatus(cookie, sonarStatus);
-
+        /*
         console.log("Starting ValueEdge Quality Gate...")
         await startQualityGate(cookie);
-
+        */
 
         
 
